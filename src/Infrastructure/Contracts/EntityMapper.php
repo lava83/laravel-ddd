65d01213fbb2ca1cf207678259cc04ff24780470
Stefan Riedel <stefan.riedel@arbeitsmedizin.de>
2026-01-25 18:25:44 +0100

    Allows returning Authenticatable model
    Updates EntityMapper to allow returning an instance of
    Authenticatable model as a possible type.
    
    This is in anticipation of supporting user authentication.

--- src/Infrastructure/Contracts/EntityMapper.php
+++ src/Infrastructure/Contracts/EntityMapper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lava83\LaravelDdd\Infrastructure\Contracts;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Lava83\LaravelDdd\Domain\Entities\Entity;
 use Lava83\LaravelDdd\Infrastructure\Models\Model;
 
@@ -11,5 +12,5 @@ interface EntityMapper
 {
     public static function toEntity(Model $model, bool $deep = false): Entity;
 
-    public static function toModel(Entity $entity): Model;
+    public static function toModel(Entity $entity): Model|Authenticatable;
 }
